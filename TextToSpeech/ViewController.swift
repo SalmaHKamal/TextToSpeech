2d154a5654fb2a79830bb93e69acdc6a26cba535
Salma Hassan <salmahassan087@gmail.com>
2019-09-13 12:18:02 +0200

    Text To Speech

--- TextToSpeech/ViewController.swift
+++ TextToSpeech/ViewController.swift
@@ -7,14 +7,28 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+   @IBOutlet weak var textView: UITextView!
    override func viewDidLoad() {
       super.viewDidLoad()
-      // Do any additional setup after loading the view.
    }
 
-
+   
+   @IBAction func onReadBtnPressed(_ sender: Any) {
+      // Line 1. Create an instance of AVSpeechSynthesizer.
+      let speechSynthesizer = AVSpeechSynthesizer()
+      // Line 2. Create an instance of AVSpeechUtterance and pass in a String to be spoken.
+      let speechUtterance: AVSpeechUtterance = AVSpeechUtterance(string: textView.text)
+      //Line 3. Specify the speech utterance rate. 1 = speaking extremely the higher the values the slower speech patterns. The default rate, AVSpeechUtteranceDefaultSpeechRate is 0.5
+//      speechUtterance.rate = AVSpeechUtteranceMaximumSpeechRate / 4.0
+      // Line 4. Specify the voice. It is explicitly set to English here, but it will use the device default if not specified.
+      speechUtterance.voice = AVSpeechSynthesisVoice(language: "en-US")
+      // Line 5. Pass in the urrerance to the synthesizer to actually speak.
+      speechSynthesizer.speak(speechUtterance)
+   }
+   
 }
 
